2fbb11b655aba8c938dd433851c7ff0c690a53f7
Ali <ali@gmail.com>
2024-08-27 19:58:44 +0500

    update contact page

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@ Route::get('/about', function(){
 	return view('welcome');
 });
 
+Route::get('/contact', function(){
+    return view('welcome');
+});
+
 Route::get('/bugFix', function(){
         return view('welcome');
 });
